6505b0e1e03ad5fd9456fa9e2bbd4006450e187a
athulmathrw27 <athulmathew27@gmail.com>
2020-12-09 10:43:25 +0530

    paging details added

--- routes/existing_service.js
+++ routes/existing_service.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const http = require("https");
 const db = require("../config/db_conn");
 const { tokenGen } = require("../config/tokenGen");
+var querystring = require('querystring');
 
 router.get('/',(req,res)=>{
     db.query("select * from zayoapi where 1",(err,result)=>{
@@ -12,37 +13,45 @@ router.get('/',(req,res)=>{
         else{
             if(result){
                 if(result[0]){
+                    var pagingdata = '{"filter": {"status":"Active" },"paging": {"top": 10,"skip": 0 }}';
+
                     var options = {
                         host: 'api.zayo.com',
                         path: '/services/service-management/v1/existing-services',
                         method: 'POST',
                         headers: {
-                          'Content-Type': 'application/json',
-                          'Content-Length': 0,
+                          'Content-Type': 'text/plain',
+                          'Content-Length': pagingdata.length,
                           'Authorization' : result[0].access_token
                         }
                     };
+
                     var httpreq = http.request(options, function (response) {
                         response.setEncoding('utf8');
                         response.on('data', function (chunk) {
-                        //   console.log("body: " + chunk);
-                        if(response.statusCode == 401){
-                            tokenGen(function(returnValue){
-                                if(returnValue){
-                                    db.query("update zayoapi set access_token = ? where id = ?",[returnValue, result[0].id ],(err, result)=>{
-                                        if(err){
-                                            res.send("unable to updte")
-                                        }
-                                        else{
-                                            console.log("updated")
-                                            res.send("new token generated")
-                                        }
-                                    })
-                                }
-                            });
+                        console.log("body: " + chunk);
+                        if(response.statusCode == 200){
+                            res.send("Approved")
                         }
                         else{
-                            res.send("Approved User")
+                            if(response.statusCode == 401){
+                                tokenGen(function(returnValue){
+                                    if(returnValue){
+                                        db.query("update zayoapi set access_token = ? where id = ?",[returnValue, result[0].id ],(err, result)=>{
+                                            if(err){
+                                                res.send("unable to updte")
+                                            }
+                                            else{
+                                                console.log("updated")
+                                                res.send("new token generated")
+                                            }
+                                        })
+                                    }
+                                });
+                            }
+                            else{
+                                res.send("something went wrong"+response.statusCode).status(response.statusCode)
+                            }
                         }
                         });
                         response.on('end', function() {
@@ -50,6 +59,7 @@ router.get('/',(req,res)=>{
                           console.log("ok access token checker ")
                         })
                     });
+                    httpreq.write(pagingdata)
                     httpreq.end();
                 }
                 else{
